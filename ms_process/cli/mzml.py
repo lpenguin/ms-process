49d0e40d98d7d3053c4031af61bbccd70b6c0047
Nikita <iamlilacpenguin@gmail.com>
2018-04-09 12:35:34 +0300

    added resampler step argument, added default parameters print

--- ms_process/cli/mzml.py
+++ ms_process/cli/mzml.py
@@ -7,9 +7,10 @@ def main():
     parser = argparse.ArgumentParser()
     subparsers = parser.add_subparsers(dest='command')
 
-    filter_parser = subparsers.add_parser('filter')
-    filter_parser.add_argument('--threshold-multiplier', default=3, type=int)
+    filter_parser = subparsers.add_parser('filter', formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+    filter_parser.add_argument('--threshold-multiplier', default=3, type=int, help='electric noise filter threshold')
     filter_parser.add_argument('--central-mz', '-c', type=float, default=800, help='central mz in resampler')
+    filter_parser.add_argument('--step', type=float, default=0.0043, help='step in resampler')
     filter_parser.add_argument('in_file')
     filter_parser.add_argument('out_file')
 
@@ -20,6 +21,7 @@ def main():
             args.out_file,
             threshold_multiplier=args.threshold_multiplier,
             central_mz=args.central_mz,
+            resampler_step=args.step,
         )
 
 
--- ms_process/mzml.py
+++ ms_process/mzml.py
@@ -10,25 +10,12 @@ from ms_process.processing.filters import ElectricNoiseFilter, ResamplerFilter, 
 def process_file(in_filename: str,
                  out_filename: str,
                  threshold_multiplier: int,
+                 resampler_step: float,
                  central_mz: float):
-    # if mz_min_max is None:
-    #     sys.stderr.write("Calculating mz min:max...\n")
-    #     spectrum_processor = SpectrumIterator()
-    #     min_mz = np.inf
-    #     max_mz = -np.inf
-    #
-    #     for i, spectrum in enumerate(spectrum_processor.process(in_filename)):
-    #         if spectrum.ms_level != 1:
-    #             continue
-    #         min_mz = min(min_mz, spectrum.mz.data.min())
-    #         max_mz = max(max_mz, spectrum.mz.data.max())
-    # else:
-    #     min_mz, max_mz = mz_min_max
-    # sys.stderr.write("min mz: {min_mz}, max_mz: {max_mz}\n".format(min_mz=min_mz, max_mz=max_mz))
     sys.stderr.write("Processing data...\n")
     filters = [
         ElectricNoiseFilter(threshold_multiplier=threshold_multiplier),
-        ResamplerFilter(sampling_rate=0.0043, central_mz=central_mz),
+        ResamplerFilter(sampling_rate=resampler_step, central_mz=central_mz),
         SGolayFilter(window_length=11, polyorder=4),
         AsFloat32Filter(),
     ]
